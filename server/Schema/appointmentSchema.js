8918fa6e96d3a64ea6f05b01596f448eb3088780
Dipika K.C <dipikak0323@gmail.com>
2025-03-22 20:31:50 +0545

    appointment schema

---
+++ server/Schema/appointmentSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  status: { 
+    type: String, 
+    enum: ["pending", "confirmed", "rejected", "canceled"], 
+    default: "pending" 
+  },
+  notes: { type: String },
+  reason: { type: String, required: true }
+}, { timestamps: true });
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = {Appointment};
